c351f95ebf9ad2679307e1f6a99fd0a14b4954c7
Voland1807 <152168547+Voland1807@users.noreply.github.com>
2025-05-13 21:02:14 +0200

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const App = () => <div>Zulieferer-Datenbank erfolgreich geladen!</div>
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
