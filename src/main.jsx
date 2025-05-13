60947ea479ccd96d9efe116c6b930d32b31cb7f8
Voland1807 <152168547+Voland1807@users.noreply.github.com>
2025-05-14 00:27:21 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App' // Das ist entscheidend!
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import AdminUpload from "./AdminUpload";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/admin-upload" element={<AdminUpload />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
-)
+);
